b42729a80b6cc878c77611ebda306fb6d2e1a4a9
shonimoni <72787020+shonimoni@users.noreply.github.com>
2020-10-16 19:17:31 +0530

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,7 +1,8 @@
 > household_power_consumption <- read.csv("C:/Users/monica/Desktop/household_power_consumption.txt", header=FALSE, sep=";", stringsAsFactors=TRUE)
 > View(household_power_consumption)
-> subSetData <- household_power_consumption[household_power_consumption$Date %in% c("1/2/2007","2/2/2007") ,]
-> globalActivePower <- as.numeric(subSetData$Global_active_power)
+> sub_data <- household_power_consumption[household_power_consumption$Date %in% c("1/2/2007","2/2/2007") ,]
+> date_time <- strptime(paste(sub_data$Date, sub_data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+> globalActivePower <- as.numeric(sub_data$Global_active_power)
 > png("plot1.png", width=480, height=480)
 > hist(globalActivePower, col="red",breaks = (0:6), main="Global Active Power", xlab="Global Active Power (kilowatts)")
 > dev.off()
